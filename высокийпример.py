d3c7c6c2df4ca9fa29a69757e4cee714c831023d
NatashaKandakova <natasha.kandakova1@yandex.ru>
2017-04-09 17:24:36 +0500

    Программирвание линейных алгоритмов
    Арифметические выражения. Высокий уровень.

---
+++ высокийпример.py
@@ -0,0 +1,9 @@
+import math
+x= float (input('x= '))
+z= float (input('z= '))
+y= float (input('y= '))
+c= float (input('c= '))
+G=((math.tan((x**4)-6))-((math.cos(z+x*y))**3))/(((math.cos(x**3))**4)*(c**2))
+
+
+print (G)
---
+++ среднийпример.py
@@ -0,0 +1,6 @@
+import math
+a= float (input('a= '))
+t= float (input('t= '))
+y= float (input('y= '))
+d=(7.8*(a**2)+3.52*t)/(math.log(a+2*y)+math.exp(y))
+print (d)
